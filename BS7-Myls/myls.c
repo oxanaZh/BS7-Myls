baad4ab36c57952f5736178874037bc4e7256520
KarstenAMF <ktymann@fh-bielefeld.de>
2016-06-06 16:30:32 +0200

    options added

--- BS7-Myls/myls.c
+++ BS7-Myls/myls.c
@@ -6,10 +6,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
+#include <unistd.h>
 #define MAX_PATH 1024
 
 char path[MAX_PATH];
-void *readPath(char *path);
+void *readPath(char *path, int, int);
 int main(int argc, char *argv[]){
 	if(argc > 1){
 		strcpy(path, argv[1]);
@@ -18,10 +19,26 @@ int main(int argc, char *argv[]){
 
 	}
 	printf("input path: %s", path);
-	readPath(path);
+	int c;
+	int aoption = 0;
+	int loption = 0;
+	 while ( (c = getopt(argc, argv, "al")) != -1) {
+	        switch (c) {
+	        case 'a':
+	        	aoption = 1;
+	        	break;
+	        case 'l':
+	        	loption = 1;
+	        	break;
+
+	        }
+	    }
+
+
+	readPath(path,aoption,loption);
 	return 0;
 }
-void *readPath(char *path){
+void *readPath(char *path,int aoption, int loption){
 	char resolved_path[MAX_PATH];
 	DIR *dir = NULL;
 	struct dirent *dptr = NULL;
@@ -30,8 +47,10 @@ void *readPath(char *path){
 		printf("resolved_path: %s\n", resolved_path);
 		if((dir = opendir(resolved_path))){
 			while((dptr = readdir(dir))){
+				if(!aoption){
 				if(strncmp(dptr->d_name, dot,1) == 0  ){
 					continue;
+				}
 				}
 				printf("%s\n",dptr->d_name);
 
