ae1f064aa7c700f96cd142bd4b8eea6ae9ccd501
ozhurakovskaya <oxana.zhurakovskaya@fh-bielefeld.de>
2016-06-06 16:04:56 +0200

    path auslesen

--- BS7-Myls/myls.c
+++ BS7-Myls/myls.c
@@ -2,8 +2,21 @@
  * myls.c
  *
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_PATH 1024
+
+char path[MAX_PATH];
 
 int main(int argc, char *argv[]){
+	if(argc > 1){
+		strcpy(path, argv[1]);
+	} else {
+		getcwd(path, MAX_PATH);
 
+	}
+	printf("input path: %s", path);
 	return 0;
 }
